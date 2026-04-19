3fa98c3467e62174a7bfbf911b006bedee65a77a
Soumalya Dhar <96789921+soumo-github@users.noreply.github.com>
2026-04-19 23:46:44 +0530

    Update IdempotencyStore.java

--- src/main/java/org/example/Model/IdempotencyStore.java
+++ src/main/java/org/example/Model/IdempotencyStore.java
@@ -7,9 +7,9 @@ public class IdempotencyStore {
 
     private final Set<Integer> processedIds = ConcurrentHashMap.newKeySet();
 
-    public boolean isProcessed(int id){
-        return processedIds.contains(id);
-    }
+    // public boolean isProcessed(int id){
+    //     return processedIds.contains(id);
+    // }
 
     public boolean markIfNotProcessed(int id){
         return processedIds.add(id);
